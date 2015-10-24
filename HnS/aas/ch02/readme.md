daa226172583d37e8632cb7fc467f817ea2a23b2
Steven Troxler <steven.troxler@gmail.com>
2015-10-24 14:25:44 -0400

    Finish chapter 2 of AAS, intro to spark
    The code cannot be run as a batch job, but the readme now describes
    how to build (using grade) and run all the analyses of the chapter.

--- HnS/aas/ch02/readme.md
+++ HnS/aas/ch02/readme.md
@@ -3,9 +3,13 @@
 To download the data into a `_data` directory inside the working directory
 (which .gitignore will ignore) run `bash get_data.sh`.
 
+## Building
+
 To build the code for the project, you can run `gradle build` from this
 directory. If you only want to access the code from the spark shell, you could
 also just boot up the shell and use the `:load` command.
+
+## Loading in an interactive shell after building
 
 To run an interactive spark shell against the build, run
 ```
@@ -13,3 +17,19 @@ spark-shell --jars build/libs/ch02.jar
 ```
 and remember that to use any of the classes defined, you need to either 
 `import ch2._` or include the prefix for anything you use.
+
+It is also possible to `:load` the code from the shell, bypassing the
+build. But the scala interpreter does not respect packages, so this can
+be a little problematic.
+
+## Running
+
+Most of the code is intended to be runnable from inside the shell (there
+is no batch job defined here, only functions). The highest-level
+function, which corresponds to the last page in Chapter 2 (except that I
+was lazy and only loaded one data file, not all of them), is the
+`apply` method in the `Analysis` class.
+
+To run it, after loading the spark shell witht the jar you can just run ```
+ch2.Analysis(sc)
+```
--- HnS/aas/ch02/src/main/scala/ch2/Ch2.scala
+++ HnS/aas/ch02/src/main/scala/ch2/Ch2.scala
@@ -1,20 +1,18 @@
 package ch2
 
 import org.apache.spark.util.StatCounter
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.RDD
 
-class NAStatCounter extends Serializable {
+/* Class that mimics the behavior of Spark's StatCounter, but also
+ * filters out NaN's and tracks how many of them we see.
+ */
+class NaStatCounter extends Serializable {
 
   val stats: StatCounter = new StatCounter()
   var missing: Long = 0
 
-  // This method mutates state. I'm keeping for consistency with the book,
-  // but unless performance considerations arise it is more scala-like to
-  // have a method like this return a new instance and make the instances
-  // immutable.
-  //
-  // Note that if we are going to use mutation, it's idiomatic to return
-  // a reference to this, allowing so-called 'fluent interfaces'
-  def add(x: Double): NAStatCounter = {
+  def add(x: Double): NaStatCounter = {
     if (x.isNaN()) {
       missing += 1
     } else {
@@ -23,7 +21,7 @@ class NAStatCounter extends Serializable {
     this
   }
 
-  def merge(other: NAStatCounter): NAStatCounter = {
+  def merge(other: NaStatCounter): NaStatCounter = {
     stats.merge(other.stats)
     missing += other.missing
     this
@@ -32,6 +30,100 @@ class NAStatCounter extends Serializable {
   override def toString = "stats: " + stats.toString + " NaN: " + missing
 }
 
-object NAStatCounter extends Serializable {
-  def apply(x: Double) = new NAStatCounter().add(x)
+object NaStatCounter extends Serializable {
+  def apply() = new NaStatCounter()
 }
+
+/* Case class corresponding to one row of the linkage data */
+case class MatchData(id1: Int, id2: Int, scores: Array[Double],
+                     matches: Boolean)
+
+object DataUtils {
+
+  def loadData(sc: SparkContext, dataPath: String = "_data/block_1.csv") = {
+
+    def isHeader(line: String) = line.contains("id_1")
+
+    def toDoubleWithNaN(num: String) = {
+        if (num.equals("?")) Double.NaN else num.toDouble
+    }
+
+    def lineToMD(line: String) = {
+      val parts = line.split(',')
+      val id1 = parts(0).toInt
+      val id2 = parts(1).toInt
+      val scores = parts.slice(2, 11).map(toDoubleWithNaN)
+      val matches = parts(11).toBoolean
+      MatchData(id1, id2, scores, matches)
+    }
+
+    (sc.textFile(dataPath)
+       .filter(!isHeader(_))
+       .map(lineToMD))
+  }
+
+  def matchCounts(mds: RDD[MatchData]) = {
+      (mds.map(_.matches)
+          .countByValue()
+          .toSeq
+          .sortBy(_._2)
+          .reverse
+          .foreach(println))
+  }
+
+  /* Compute an array of NaStatCounter, corresponding to each variable
+   * in an RDD of arrays of observations.
+   */
+  def statsWithMissing(rdd: RDD[Array[Double]]): Array[NaStatCounter] = {
+
+      // Implementation Not:
+      //   We use mapPartition here for efficiency: when dealing with mutable
+      //   data processing classes like NaStatCounter, you avoid a great deal
+      //   of gc overhead by processing in batches and then merging, rather
+      //   than processing one element at a time and merging.
+
+      // For mapPartition to work, both the input and output must be an
+      // iterator. In our case, we only need to output an iterator of length
+      // one ...we aren't converting the array to an iterator, we are wrapping
+      // it: the output is Iterator[Array[NaStatCounter]]
+      //
+      // I had to modify the code in the book to make it run on a smaller
+      // dataset, because ins ome cases the partition can be empty after
+      // filtering, leading to the next() call raising an exception.
+      def computeNaStatsOnPartition(iter: Iterator[Array[Double]]) = {
+          val nastats = (0 until 9).map(i => NaStatCounter()).toArray
+          iter.foreach(nastats.zip(_).foreach {
+              case (nastat, num) => nastat.add(num)
+          })
+          Iterator(nastats)
+      }
+
+      // There are no iterators here; after the mapPartition, everything
+      // gets flattened back out by spark.
+      def mergePartitions(nastats1: Array[NaStatCounter],
+                          nastats2: Array[NaStatCounter]) = {
+          nastats1.zip(nastats2) .map { case (ns1, ns2) => ns1.merge(ns2) }
+      }
+
+
+      val nastatsOnPartitions = rdd.mapPartitions(computeNaStatsOnPartition)
+      val nastats = nastatsOnPartitions.reduce(mergePartitions)
+      nastats
+  }
+}
+
+object Analysis {
+
+    def apply(sc: SparkContext) = {
+        val mds = DataUtils.loadData(sc)
+        val ymatches = mds.filter(_.matches)
+        val nmatches = mds.filter(!_.matches)
+        val ystats = DataUtils.statsWithMissing(ymatches.map(_.scores))
+        val nstats = DataUtils.statsWithMissing(nmatches.map(_.scores))
+        ystats.zip(nstats).map (
+            { case (y, n) => (y.missing+n.missing, y.stats.mean-n.stats.mean) }
+        )
+    }
+}
+
+
